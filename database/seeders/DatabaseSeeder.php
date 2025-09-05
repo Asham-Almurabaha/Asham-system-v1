8b513e3794583febc83d5cab1accfe7a1a180ff0
Mohamed saeed <asham-almurabaha@gmail.com>
2025-09-06 00:10:56 +0300

    التعديل

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        
         $this->call([
             PermissionsSeeder::class,
             NationalitySeeder::class,
             TitleSeeder::class,
         ]);
-
+        
+        // User::factory(10)->create();
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
